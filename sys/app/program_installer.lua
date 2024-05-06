73d3b37625a2f9e95aeaf35e3757f2d3f12b7286
Tavȳza <120181836+Tavyza@users.noreply.github.com>
2024-05-06 12:12:55 -0400

    fixed program installer?

--- sys/app/program_installer.lua
+++ sys/app/program_installer.lua
@@ -1,25 +1,45 @@
-term = require("term")
-term.clear()
-print("Note: You need an internet card to use this program.")
-print("1-pastebin")
-print("2-github")
+local t = require("term")
+local ct = require("centerText")
+local e = require("event")
 
-io.write("-> ")
-local choice = io.read()
+t.clear()
 
-if choice == "1" then
-  io.write("pastebin code: ")
-  local pastebinCode = io.read()
-  io.write("file name: ")
-  local filename = io.read()
-  os.execute("pastebin get " .. pastebinCode .. " " .. filename)
-  os.execute(main)  
-elseif choice == "2" then
-  io.write("raw github link (click raw on the chosen file): ")
-  local githubCode = io.read()
-  os.execute("wget " .. githubCode)
-  os.execute(main)  
-else
-  print("not a valid option.")
-  os.execute("program_installer")
+local function menu()
+  local options = {"Install from Pastebin", "Install from GitHub"}
+  t.clear()
+  ct(1, "-- Program installer --", 0xFFFFFF)
+  for i, options in ipairs(options) do
+    ct(5 + (i - 1) * 2, options, 0xFFFFFF)
+  end
+end
+
+local function pastebin()
+  print("Enter pastebin code")
+  io.write("-> ")
+  code = io.read()
+  print("Enter file name (leave blank to keep original name)")
+  name = io.read()
+  os.execute("pastebin get " .. code .. " " .. name)
+  os.exit()
+end
+local function github()
+  print("Enter raw github link")
+  io.write("-> ")
+  link = io.read()
+  print("Enter file name (leave blank to keep original name)")
+  gName = io.read()
+  os.execute("wget " .. link .. " " .. gName)
+  os.exit()
+end
+
+
+while true do
+  local _, _, _, y, _, _ = e.pull("touch")
+  local choice = math.floor((y - 5) / 2) + 1
+  if choice == 1 then
+    pastebin()
+  elseif choice == 2 then
+    github()
+  end
+  menu()
 end
