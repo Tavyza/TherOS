ad4ce605c37214178e38f1bc9b7d1a35e85375ec
Tavȳza <120181836+Tavyza@users.noreply.github.com>
2024-06-10 09:55:13 -0400

    Update installer.luaaaaaaaa (1.0.3)

--- sys/app/installer.lua
+++ sys/app/installer.lua
@@ -1,3 +1,3 @@
 print("Installer path has changed in version 1.0.3! pulling...")
 
-os.execute("wget -f -q https://raw.githubusercontent.com/Tavyza/TherOS/main/sys/apps/installer.lua")
+os.execute("wget -f -q https://raw.githubusercontent.com/Tavyza/TherOS/main/sys/apps/installer.lua /sys/apps/installer.lua")
