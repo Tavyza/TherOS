a4c587818b119397f4f3c3ca3b517640fd4ab6d0
exolotyll <166331205+exolotyll@users.noreply.github.com>
2024-06-21 15:48:18 -0400

    gravy boat 1.1.1

--- sys/env/newmain.lua
+++ sys/env/newmain.lua
@@ -3,8 +3,18 @@ local term = require("term")
 local component = require("component")
 local gpu = component.gpu
 
+local options = {}
+
+table.insert(options, "test option one")
+table.insert(options, "test option two")
+table.insert(options, "test option three")
+
 while true do
     local event = table.pack(event.pull())
-    gpu.setBackground(0x0D00B3)
-    term.clear()
+    local w, h = gpu.getResolution()
+    gpu.fill(1, 1, w, h, " ")
+
+    for _, option in ipairs(options) do
+        io.write(option)
+    end
 end
