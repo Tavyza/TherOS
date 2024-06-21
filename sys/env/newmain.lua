df8e5aa3281e85fe41eac30a51dcdc3cb9f9a2fa
exolotyll <166331205+exolotyll@users.noreply.github.com>
2024-06-21 15:38:28 -0400

    gravy boat 1.1.1

--- sys/env/newmain.lua
+++ sys/env/newmain.lua
@@ -1,1 +1,10 @@
+local event = require("event")
+local term = require("term")
+local component = require("component")
+local gpu = component.gpu
 
+while true do
+    local event = table.pack(event.pull())
+    gpu.setBackground(0x0D00B3)
+    term.clear()
+end
