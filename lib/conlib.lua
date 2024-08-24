24a025feb0d3006b3d8a2865b38e076dbb515925
Tavyza <icelandicbread@outlook.com>
2024-08-24 10:24:39 -0400

    fix various issues with the config

--- lib/conlib.lua
+++ lib/conlib.lua
@@ -1,57 +1,48 @@
--- config for TherOS
-config = {}
-configfile = io.read("/sys/.config/general.tc", "r")
+-- conlib for TherOS
+conlib = {}
+configfile = io.open("/sys/.config/general.tc", "r")
 fullfile = configfile:read("*a")
-
-config.bkgclr = tonumber(fullfile:match("Bck-clr:%s*(.+)"))
-
-config.txtclr = tonumber(fullfile:match("Txt-clr:%s*(.+)"))
-
--- 
-config.usoclr = tonumber(fullfile:match("Usl-clr:%s*(.+)"))
-
--- selected option color (for apps that support it)
-config.sloclr = tonumber(fullfile:match("Sel-clr:%s*(.+)"))
-
--- file manager dir color
-config.fmdclr = tonumber(fullfile:match("Fmd-clr:%s*(.+)"))
-
--- file manager file color
-config.fmfclr = tonumber(fullfile:match("Fmf-clr:%s*(.+)"))
-
--- apps directory (ABSOLUTE PATH ONLY)
-config.appdir = fullfile:match("App-dir:%s*(.+)")
-
--- terminal location (ABSOLUTE PATH ONLY)
-config.trmdir = fullfile:match("Trm-dir:%s*(.+)")
-
--- default editor (ABSOLUTE PATH ONLY)
-config.editor = fullfile:match("Edt-dir:%s*(.+)")
-
-function config.general()
-  return config.bkgclr, config.txtclr, config.usoclr, config.sloclr, config.fmdclr, config.fmfclr, config.appdir, config.trmdir, config.editor
+configfile:close()
+general = {}
+for line in fullfile:gmatch("[^\r\n]+") do
+  table.insert(general, line)
 end
--- versions for various system utilities, basically in what version they were updated last
-<<<<<<< HEAD
-config.sysver = fullfile:match("System:%s*(.+)")
-config.inver = fullfile:match("Instll:%s*(.+)") -- installer version
-config.fmver = fullfile:match("Filemn:%s*(.+)") -- file manager version
-config.ctlibver = fullfile:match("Ctxlib:%s*(.+)") -- center text library version
-config.configver = fullfile:match("Config:%s*(.+)")
-config.theroslibver = fullfile:match("ThOlib:%s*(.+)")
-config.termver = fullfile:match("Termin:%s*(.+)")
-=======
-config.sysver = "1.1.3-B"
-config.inver = "1.1.3-B" -- installer version
-config.fmver = "1.1.3-B" -- file manager version
-config.ctlibver = "1.1.2-B" -- center text library version
-config.configver = "1.1.2-B"
-config.theroslibver = "1.1.2-B"
-config.termver = "1.0.0"
->>>>>>> 500e8a525261ee6e231da288c1fd80a745bdad63
-
-function config.version()
-  return config.sysver, config.inver, config.fmver, config.ctlibver, config.configver, config.theroslibver, config.termver
+for _, line in ipairs(general) do
+  conlib.bkgclr = conlib.bkgclr or tonumber(line:match("Bck%-clr:%s*(.+)"))
+  conlib.txtclr = conlib.txtclr or tonumber(line:match("Txt%-clr:%s*(.+)"))
+  conlib.usoclr = conlib.usoclr or tonumber(line:match("Usl%-clr:%s*(.+)"))
+  conlib.sloclr = conlib.sloclr or tonumber(line:match("Sel%-clr:%s*(.+)"))
+  conlib.fmdclr = conlib.fmdclr or tonumber(line:match("Fmd%-clr:%s*(.+)"))
+  conlib.fmfclr = conlib.fmfclr or tonumber(line:match("Fmf%-clr:%s*(.+)"))
+  conlib.appdir = conlib.appdir or line:match("App%-dir:%s*(.+)")
+  conlib.trmdir = conlib.trmdir or line:match("Trm%-dir:%s*(.+)")
+  conlib.editor = conlib.editor or line:match("Edt%-dir:%s*(.+)")
 end
 
-return config
+function conlib.general()
+  return conlib.bkgclr, conlib.txtclr, conlib.usoclr, conlib.sloclr, conlib.fmdclr, conlib.fmfclr, conlib.appdir, conlib.trmdir, conlib.editor
+end
+configfile = io.open("/sys/.config/version.tc")
+fullfile = configfile:read("*a")
+configfile:close()
+version = {}
+for line in fullfile:gmatch("[^\r\n]+") do
+  table.insert(version, line)
+end
+
+for _, line in ipairs(version) do
+  conlib.sysver = conlib.sysver or line:match("System:%s*(.+)")
+  conlib.inver = conlib.inver or line:match("Instll:%s*(.+)")
+  conlib.fmver = conlib.fmver or line:match("Filemn:%s*(.+)")
+  conlib.ctlibver = conlib.ctlibver or line:match("Ctxlib:%s*(.+)")
+  conlib.configver = conlib.configver or line:match("Config:%s*(.+)")
+  conlib.theroslibver = conlib.theroslibver or line:match("ThOlib:%s*(.+)")
+  conlib.termver = conlib.termver or line:match("Termin:%s*(.+)")
+end
+
+function conlib.version()
+  return conlib.sysver, conlib.inver, conlib.fmver, conlib.ctlibver, conlib.configver, conlib.theroslibver, conlib.termver
+end
+
+return conlib
+
