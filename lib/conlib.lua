0bb3c23cdeb7082dd05e2204a07534ba46192488
Tavyza <icelandicbread@outlook.com>
2024-08-24 10:28:05 -0400

    push this god damn conlib

--- lib/conlib.lua
+++ lib/conlib.lua
@@ -8,15 +8,15 @@ for line in fullfile:gmatch("[^\r\n]+") do
   table.insert(general, line)
 end
 for _, line in ipairs(general) do
-  conlib.bkgclr = conlib.bkgclr or tonumber(line:match("Bck%-clr:%s*(.+)"))
-  conlib.txtclr = conlib.txtclr or tonumber(line:match("Txt%-clr:%s*(.+)"))
-  conlib.usoclr = conlib.usoclr or tonumber(line:match("Usl%-clr:%s*(.+)"))
-  conlib.sloclr = conlib.sloclr or tonumber(line:match("Sel%-clr:%s*(.+)"))
-  conlib.fmdclr = conlib.fmdclr or tonumber(line:match("Fmd%-clr:%s*(.+)"))
-  conlib.fmfclr = conlib.fmfclr or tonumber(line:match("Fmf%-clr:%s*(.+)"))
-  conlib.appdir = conlib.appdir or line:match("App%-dir:%s*(.+)")
-  conlib.trmdir = conlib.trmdir or line:match("Trm%-dir:%s*(.+)")
-  conlib.editor = conlib.editor or line:match("Edt%-dir:%s*(.+)")
+  conlib.bkgclr = conlib.bkgclr or tonumber(line:match("Bck%-clr:(.+)"))
+  conlib.txtclr = conlib.txtclr or tonumber(line:match("Txt%-clr:(.+)"))
+  conlib.usoclr = conlib.usoclr or tonumber(line:match("Usl%-clr:(.+)"))
+  conlib.sloclr = conlib.sloclr or tonumber(line:match("Sel%-clr:(.+)"))
+  conlib.fmdclr = conlib.fmdclr or tonumber(line:match("Fmd%-clr:(.+)"))
+  conlib.fmfclr = conlib.fmfclr or tonumber(line:match("Fmf%-clr:(.+)"))
+  conlib.appdir = conlib.appdir or line:match("App%-dir:(.+)")
+  conlib.trmdir = conlib.trmdir or line:match("Trm%-dir:(.+)")
+  conlib.editor = conlib.editor or line:match("Edt%-dir:(.+)")
 end
 
 function conlib.general()
@@ -31,13 +31,13 @@ for line in fullfile:gmatch("[^\r\n]+") do
 end
 
 for _, line in ipairs(version) do
-  conlib.sysver = conlib.sysver or line:match("System:%s*(.+)")
-  conlib.inver = conlib.inver or line:match("Instll:%s*(.+)")
-  conlib.fmver = conlib.fmver or line:match("Filemn:%s*(.+)")
-  conlib.ctlibver = conlib.ctlibver or line:match("Ctxlib:%s*(.+)")
-  conlib.configver = conlib.configver or line:match("Config:%s*(.+)")
-  conlib.theroslibver = conlib.theroslibver or line:match("ThOlib:%s*(.+)")
-  conlib.termver = conlib.termver or line:match("Termin:%s*(.+)")
+  conlib.sysver = conlib.sysver or line:match("System:(.+)")
+  conlib.inver = conlib.inver or line:match("Instll:(.+)")
+  conlib.fmver = conlib.fmver or line:match("Filemn:(.+)")
+  conlib.ctlibver = conlib.ctlibver or line:match("Ctxlib:(.+)")
+  conlib.configver = conlib.configver or line:match("Config:(.+)")
+  conlib.theroslibver = conlib.theroslibver or line:match("ThOlib:(.+)")
+  conlib.termver = conlib.termver or line:match("Termin:(.+)")
 end
 
 function conlib.version()
