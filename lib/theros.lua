723fa0eb3c864c16edab114862935095e0243cd3
Tavyza <icelandicbread@outlook.com>
2024-10-03 07:00:05 -0400

    1.2-B

--- lib/theros.lua
+++ lib/theros.lua
@@ -48,12 +48,8 @@ function theros.dwindow(x, y, w, h, titletext)
   gpu.set(x+w-1,y+h-1,"╝") -- bottom right corner
 
   gpu.set(math.floor(w/2)-math.floor(#titletext/2), 1, "[" .. titletext .. "]") -- setting the top text at the middle of the top
-  --gpu.setForeground(0xFF0000) --red
-  --gpu.set(w-4,1,"[X]") -- close button
-  --local _, x, y, _, _ = e.pull("touch") -- grabbing touch signal
-  --if x == w-4 and y == 1 then -- do the exit thingy
-    --os.exit()
-  --end
+  gpu.setForeground(0xFF0000) --red
+  gpu.set(w-4,1,"[X]") -- close button
   return {x = x, y = y, w = w, h = h}
 end
 
